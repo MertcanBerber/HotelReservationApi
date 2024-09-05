c341b4bb36b45cd38d719f0167de5fb17d6251fe
MertcanBerber <132483553+MertcanBerber@users.noreply.github.com>
2024-09-05 22:12:57 +0300

    Initial commit

---
+++ ApiConsume/HotelProject.EntityLayer/Concrete/Service.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelProject.EntityLayer.Concrete
+{
+    public class Service
+    {
+        [Key]
+        public int ServıceId { get; set; }
+        public string ServıceIcon { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
